88af44da0fa9df53b5159252d4ee6c642f9b3caf
91751 <91751@LAPTOP-A9N4KV2V>
2023-08-11 01:00:41 +0530

    ..

--- src/com/roomprogram/ComitCheck.java
+++
@@ -1,8 +0,0 @@
-package com.roomprogram;
-
-public class ComitCheck {
-	public static void main(String[] args) {
-		System.out.println("Lucky");
-	}
-
-}
--- src/com/roomprogram/ImmutableStringMain.java
+++
@@ -1,9 +0,0 @@
-package com.roomprogram;
-
-public class ImmutableStringMain {
-	public static void main(String[] args) {
-		ImmutableString firstString = new ImmutableString("Sufiyan");
-		  System.out.println(firstString.getValue());
-
-	}
-}
